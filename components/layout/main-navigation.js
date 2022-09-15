f196858815cc0dc098f2cc19951d904e3a3845c5
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-09-15 12:24:48 +0200

    add auth base

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,12 +1,19 @@
 import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/client';
 
 import Logo from './logo';
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const [session, loading] = useSession();
+
+  function logoutHandler() {
+    signOut();
+  }
+
   return (
     <header className={classes.header}>
-      <Link href='/'>
+      <Link href="/">
         <a>
           <Logo />
         </a>
@@ -14,11 +21,26 @@ function MainNavigation() {
       <nav>
         <ul>
           <li>
-            <Link href='/posts'>Posts</Link>
+            <Link href="/posts">Posts</Link>
           </li>
           <li>
-            <Link href='/contact'>Contact</Link>
+            <Link href="/contact">Contact</Link>
           </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
